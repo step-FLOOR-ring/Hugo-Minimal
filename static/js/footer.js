d8445c2eeb9003dfa0cd212ae55e94b12bdb056f
Adam <61597803+adamrowland02@users.noreply.github.com>
2023-04-18 21:25:15 +0100

    fixed dark mode bug with list.html
    issue with enabling light mode in list.html causing the text for dark mode to be displayed if the page is left and reopened, only fixed by clearing application cache inside dev tools, no longer an issue.

--- static/js/footer.js
+++ static/js/footer.js
@@ -1,12 +1,8 @@
 var isLightModeEnabled = JSON.parse(localStorage.getItem('isLightModeEnabled'));
 var footer = document.querySelector('.main-footer');
+var toggleButton = document.getElementById('toggle');
 
 if (isLightModeEnabled && footer) {
   footer.classList.add('light-footer');
   toggleButton.innerHTML = '🌑';
 }
-
-var toggleButton = document.getElementById('toggle');
-        if (isLightModeEnabled && footer) {
-          toggleButton.innerHTML = '🌑';
-        }
--- static/js/list.js
+++ static/js/list.js
@@ -1,6 +1,6 @@
 var isLightModeEnabled = JSON.parse(localStorage.getItem('isLightModeEnabled'));
-    var headerLink = document.querySelector('h3 a');
+var headerLink = document.querySelector('h3 > a');
 
-    if (isLightModeEnabled) {
-      headerLink.classList.add('light-mode');
-    }
+if (isLightModeEnabled) {
+  headerLink.classList.add('.light-mode');
+}
